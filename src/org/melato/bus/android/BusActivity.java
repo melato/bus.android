1c23886480733cb593352172dc0ef58efb6ba7b4
Alex Athanasopoulos <alex@melato.org>
2012-08-28 10:46:12 +0300

    use a demo GPX file.

--- src/org/melato/bus/android/BusActivity.java
+++ src/org/melato/bus/android/BusActivity.java
@@ -1,7 +1,12 @@
 package org.melato.bus.android;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
+
+import org.melato.gpx.GPX;
+import org.melato.gpx.GPXParser;
 
 import android.app.ListActivity;
 import android.os.Bundle;
@@ -12,12 +17,26 @@ import android.widget.ListView;
 public class BusActivity extends ListActivity {
   List<BusStop> stops;
   
-  List<BusStop> createDemoStops() {
+  List<BusStop> createDemoStops1() {
     BusStop[] stops = new BusStop[80];
     for( int i = 0; i < stops.length; i++ ) {
       stops[i] = new BusStop("Stop " + i );
     }
     return Arrays.asList(stops);
+  }
+  
+  List<BusStop> createDemoStops() {
+    try {
+      GPXParser parser = new GPXParser();
+      InputStream input = getClass().getResourceAsStream("data/x051-1.gpx");
+      if ( input == null ) {
+        throw new RuntimeException( "Cannot find resource." );
+      }
+      GPX gpx = parser.parse( input );
+      return BusStop.listFromGPX(gpx);      
+    } catch(IOException e) {
+      throw new RuntimeException(e);
+    }
   }
   
   public BusActivity() {
--- src/org/melato/bus/android/BusStop.java
+++ src/org/melato/bus/android/BusStop.java
@@ -1,5 +1,11 @@
 package org.melato.bus.android;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.melato.gpx.GPX;
+import org.melato.gpx.Waypoint;
+
 public class BusStop {
   private String title;
   
@@ -8,9 +14,21 @@ public class BusStop {
     this.title = title;
   }
 
+  public BusStop(Waypoint waypoint) {
+    title = waypoint.getName();
+  }
   @Override
   public String toString() {
     return title;
+  }
+  
+  public static List<BusStop> listFromGPX(GPX gpx) {
+    List<Waypoint> waypoints = gpx.getRoutes().get(0).path.getWaypoints();
+    BusStop[] stops = new BusStop[waypoints.size()];
+    for( int i = 0; i < stops.length; i++ ) {
+      stops[i] = new BusStop(waypoints.get(i));
+    }
+    return Arrays.asList(stops);
   }
   
 }
