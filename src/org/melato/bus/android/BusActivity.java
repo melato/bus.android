a62bc53741673047fa2a8dd443ffc24d17aace7f
Alex Athanasopoulos <alex@melato.org>
2012-08-28 13:43:57 +0300

    added package

--- src/org/melato/bus/android/BusActivity.java
+++ src/org/melato/bus/android/BusActivity.java
@@ -1,39 +1,55 @@
 package org.melato.bus.android;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
 import java.util.List;
 
+import org.melato.bus.util.BusStop;
 import org.melato.gpx.GPX;
 import org.melato.gpx.GPXParser;
+import org.melato.gpx.Point;
+import org.melato.gpx.util.ProximityFinder;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
-public class BusActivity extends ListActivity {
+public class BusActivity extends ListActivity implements LocationListener {
   List<BusStop> stops;
+  int   closestStop = -1;
+  private Point location;
   
-  List<BusStop> createDemoStops1() {
-    BusStop[] stops = new BusStop[80];
-    for( int i = 0; i < stops.length; i++ ) {
-      stops[i] = new BusStop("Stop " + i );
+  List<BusStop> readStops(InputStream input) {
+    if ( input == null ) {
+      throw new RuntimeException( "Cannot find resource." );
     }
-    return Arrays.asList(stops);
-  }
-  
-  List<BusStop> createDemoStops() {
     try {
       GPXParser parser = new GPXParser();
-      InputStream input = getClass().getResourceAsStream("data/x051-1.gpx");
-      if ( input == null ) {
-        throw new RuntimeException( "Cannot find resource." );
-      }
       GPX gpx = parser.parse( input );
       return BusStop.listFromGPX(gpx);      
+    } catch(IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  List<BusStop> createDemoStops() {
+    return readStops( new File( "/sdcard/bus/x409-1.gpx"));
+  }
+  
+  List<BusStop> readStops(File file) {
+    try {
+      InputStream input = new FileInputStream(file);
+      return readStops(input);
     } catch(IOException e) {
       throw new RuntimeException(e);
     }
@@ -47,6 +63,13 @@ public class BusActivity extends ListActivity {
   public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setStops(createDemoStops());
+      LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+      locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000L, 100f, this );
+      locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 5f, this);
+      Location last = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+      if ( last == null )
+        last = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+      setLocation(last);      
   }
 
   @Override
@@ -54,9 +77,81 @@ public class BusActivity extends ListActivity {
     super.onListItemClick(l, v, position, id);
   }
 
+  class BusListAdapter extends ArrayAdapter<BusStop> {
+    public BusListAdapter() {
+      super(BusActivity.this, R.layout.bus_stop_item, stops);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+      convertView = super.getView(position, convertView, parent );
+      TextView view = (TextView) convertView;
+      String text = stops.get(position).toString();
+      if ( closestStop == position ) {
+        text = "-> " + text;
+      }
+      view.setText(text);
+      return view;
+    }
+
+    /*
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+      TextView view = (TextView) convertView;
+      String text = stops.get(position).toString();
+      if ( closestStop == position ) {
+        text = "-> " + text;
+      }
+      view.setText(text);
+      return view;
+    }
+    */
+    
+  }
   public void setStops(List<BusStop> stops) {
     this.stops = stops;
-    setListAdapter(new ArrayAdapter<BusStop>(this, R.layout.bus_stop_item,
-        stops));
+    setListAdapter(new BusListAdapter());
+    //setListAdapter( new ArrayAdapter<BusStop>(this, R.layout.bus_stop_item, stops));           
+    
+  }
+  
+  public void setLocation(Point here) {
+    if ( here == null )
+      return;
+    if ( here.equals(location)) {
+      return;
+    }
+    location = here;
+    ProximityFinder proximity = new ProximityFinder();
+    proximity.setTargetDistance(1000f);
+    proximity.setSequence(BusStop.asWaypointList(stops));
+    int size = stops.size();
+    for( int i = 0; i < size; i++ ) {
+      stops.get(i).distanceFromStart = proximity.getPathLength(i);
+    }
+    closestStop = proximity.findClosestNearby(location);
+    setStops(stops);
+  }
+  
+  
+  public void setLocation(Location loc) {
+    if ( loc == null )
+      return;
+    Point p = new Point( (float) loc.getLatitude(), (float) loc.getLongitude());
+    setLocation(p);
+  }
+  
+  
+  @Override
+  public void onLocationChanged(Location location) {
+  }
+  @Override
+  public void onProviderDisabled(String provider) {
+  }
+  @Override
+  public void onProviderEnabled(String provider) {
+  }
+  @Override
+  public void onStatusChanged(String provider, int status, Bundle extras) {
   }
 }
