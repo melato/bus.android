387827ad3a12da5d1f7113b24e8f7cc6da72d5e8
Alex Athanasopoulos <alex@melato.org>
2012-09-03 01:27:50 +0300

    removed provider-related things

--- src/org/melato/bus/android/db/RoutesDatabase.java
+++ src/org/melato/bus/android/db/RoutesDatabase.java
@@ -1,10 +1,7 @@
 package org.melato.bus.android.db;
 
-import android.net.Uri;
 
 public final class RoutesDatabase {
-  public static final String AUTHORITY = "org.melato.bus";
-  public static final Uri CONTENT_URI = Uri.parse( "content://" + RoutesDatabase.AUTHORITY );
   static final String DATABASE_NAME = "ROUTES.db";
   /** The version of the database schema */
   static final int DATABASE_VERSION = 1;
@@ -49,52 +46,20 @@ public final class RoutesDatabase {
 
   public static final class Routes extends RoutesColumns implements android.provider.BaseColumns
   {
-     public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.org.melato.bus.route";
-     public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.org.melato.bus.route";
-     public static final Uri CONTENT_URI = Uri.parse( "content://" + RoutesDatabase.AUTHORITY + "/" + Routes.TABLE );
-
      /** The name of this table */
      public static final String TABLE = "routes";
-     static final String CREATE_STATEMENT = 
-        "CREATE TABLE " + Routes.TABLE + "(" + " " + Routes._ID           + " " + Routes._ID_TYPE + 
-                                         "," + " " + Routes.NAME          + " " + Routes.NAME_TYPE + 
-                                         "," + " " + Routes.LABEL          + " " + Routes.LABEL_TYPE + 
-                                         "," + " " + Routes.TITLE          + " " + Routes.TITLE_TYPE + 
-                                         "," + " " + Routes.DIRECTION          + " " + Routes.DIRECTION_TYPE + 
-                                         ");";
   }
 
   
   public static final class Markers extends MarkersColumns implements android.provider.BaseColumns
   {
-     public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.org.melato.bus.marker";
-     public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.org.melato.bus.marker";
-     public static final Uri CONTENT_URI = Uri.parse( "content://" + RoutesDatabase.AUTHORITY + "/" + Markers.TABLE );
-
      /** The name of this table */
      public static final String TABLE = "markers";
-     static final String CREATE_STATEMENT = 
-        "CREATE TABLE " + Markers.TABLE + "(" + " " + Markers._ID           + " " + Markers._ID_TYPE + 
-                                         "," + " " + Markers.SYMBOL          + " " + Markers.SYMBOL_TYPE + 
-                                         "," + " " + Markers.NAME          + " " + Markers.NAME_TYPE + 
-                                         "," + " " + Markers.LAT          + " " + Markers.LAT_TYPE + 
-                                         "," + " " + Markers.LON          + " " + Markers.LON_TYPE + 
-                                         ");";
   }
 
   public static final class Stops extends StopsColumns implements android.provider.BaseColumns
   {
-    public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.org.melato.bus.stop";
-     public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.nl.sogeti.bus.stop";
-     public static final Uri CONTENT_URI = Uri.parse( "content://" + RoutesDatabase.AUTHORITY + "/" + Stops.TABLE );
-
      public static final String TABLE = "stops";
-     static final String CREATE_STATEMENT = 
-        "CREATE TABLE " + Stops.TABLE + "(" + " " + Stops._ID           + " " + Stops._ID_TYPE + 
-                                         "," + " " + Stops.MARKER          + " " + Stops.MARKER_TYPE + 
-                                         "," + " " + Stops.ROUTE          + " " + Stops.ROUTE_TYPE + 
-                                         "," + " " + Stops.SEQ          + " " + Stops.SEQ_TYPE + 
-                                         ");";
   }
   
 }
