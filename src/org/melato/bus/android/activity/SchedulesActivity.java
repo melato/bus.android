90b980abc06b9e12d68cf355e534dc9f64941829
Alex Athanasopoulos <alex@melato.org>
2012-11-22 08:28:24 +0200

    fixed warning

--- src/org/melato/bus/android/activity/SchedulesActivity.java
+++ src/org/melato/bus/android/activity/SchedulesActivity.java
@@ -31,7 +31,6 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
