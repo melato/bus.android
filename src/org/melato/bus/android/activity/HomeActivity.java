98bba09c7b33697bc6c4086578111f1f12ef453f
Alex Athanasopoulos <alex@melato.org>
2013-04-28 15:01:01 +0300

    trying out grid view

---
+++ src/org/melato/bus/android/activity/HomeActivity.java
@@ -0,0 +1,89 @@
+/*-------------------------------------------------------------------------
+ * Copyright (c) 2012,2013 Alex Athanasopoulos.  All Rights Reserved.
+ * alex@melato.org
+ *-------------------------------------------------------------------------
+ * This file is part of Athens Next Bus
+ *
+ * Athens Next Bus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Athens Next Bus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Athens Next Bus.  If not, see <http://www.gnu.org/licenses/>.
+ *-------------------------------------------------------------------------
+ */
+package org.melato.bus.android.activity;
+
+import org.melato.bus.android.R;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/** The main activity checks for updates and launches the next activity. */
+public class HomeActivity extends Activity {
+  /** Called when the activity is first created. */  
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.home);
+      GridView grid = (GridView) findViewById(R.id.gridView);
+      grid.setAdapter(new ImageAdapter(this));
+  }
+
+
+  public static class ImageAdapter extends BaseAdapter {
+    private Context context;
+
+    public ImageAdapter(Context c) {
+        context = c;
+    }
+
+    public int getCount() {
+        return items.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(items[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] items = {
+            R.drawable.map,
+            R.drawable.nearby
+            };
+}
+
+  
+}
