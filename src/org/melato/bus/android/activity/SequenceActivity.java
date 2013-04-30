1c5e039f87c48fba6dddf7f35d35ea15da405315
Alex Athanasopoulos <alex@melato.org>
2013-04-30 16:04:15 +0300

    .

---
+++ src/org/melato/bus/android/activity/SequenceActivity.java
@@ -0,0 +1,50 @@
+/*-------------------------------------------------------------------------
+ * Copyright (c) 2012,2013 Alex Athanasopoulos.  All Rights Reserved.
+ * alex@melato.org
+ *-------------------------------------------------------------------------
+ * This file is part of Athens Next Bus
+ *
+ * Athens Next Bus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Athens Next Bus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Athens Next Bus.  If not, see <http://www.gnu.org/licenses/>.
+ *-------------------------------------------------------------------------
+ */
+package org.melato.bus.android.activity;
+
+import org.melato.bus.android.Info;
+import org.melato.bus.android.R;
+import org.melato.bus.plan.Sequence;
+import org.melato.bus.plan.Sequence.Leg;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+/**
+ * Displays a sequence
+ * @author Alex Athanasopoulos
+ */
+public class SequenceActivity extends ListActivity {
+  Sequence sequence;
+
+  public SequenceActivity() {
+  }
+  
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    sequence = Info.getSequence();
+    setListAdapter(new ArrayAdapter<Leg>(this, R.layout.list_item, sequence.getLegs()));
+  }
+  
+}
