943834ee6c76548d77bee806726a21fd1c611912
Alex Athanasopoulos <alex@melato.org>
2014-01-12 19:44:40 +0200

    fixed launcher crash on Android 4.0.3

--- src/org/melato/bus/android/bookmark/WidgetConfigurationActivity.java
+++ src/org/melato/bus/android/bookmark/WidgetConfigurationActivity.java
@@ -1,9 +1,7 @@
 package org.melato.bus.android.bookmark;
 
-import org.melato.android.AndroidLogger;
 import org.melato.android.bookmark.SqlBookmark;
 import org.melato.client.Bookmark;
-import org.melato.log.Log;
 
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
@@ -16,16 +14,14 @@ public class WidgetConfigurationActivity extends BusBookmarksActivity {
   public void onCreate(Bundle savedInstanceState) {
     setHasContextMenu(false);
     super.onCreate(savedInstanceState);
-    Log.setLogger(new AndroidLogger(this));
     Intent intent = getIntent();
     Bundle extras = intent.getExtras();
     if (extras != null) {
         widgetId = extras.getInt(
                 AppWidgetManager.EXTRA_APPWIDGET_ID, 
                 AppWidgetManager.INVALID_APPWIDGET_ID);
-        System.out.println( "configure widgetId: " + widgetId);
     }
-    setResult(RESULT_CANCELED);    
+    //setResult(RESULT_CANCELED);    
   }
   protected void open(Bookmark bookmark) {
     if ( ! (bookmark instanceof SqlBookmark) ) {
@@ -33,9 +29,12 @@ public class WidgetConfigurationActivity extends BusBookmarksActivity {
     }
     WidgetUpdater widgetManager = new WidgetUpdater(this);
     widgetManager.addWidget(widgetId, (SqlBookmark)bookmark);
-    widgetManager.updateWidget(widgetId, bookmark);
+    widgetManager.updateWidget(widgetId, bookmark);      
   
-    setResult(RESULT_OK);
+    Intent result = new Intent();
+    // The following is needed by Android 4.0.3.
+    result.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+    setResult(RESULT_OK, result);
     finish();
   }
 }
