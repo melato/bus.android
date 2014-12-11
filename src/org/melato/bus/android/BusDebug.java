634e0b37d3334575c4539d34a90e91ce394d46e9
Alex Athanasopoulos <alex@melato.org>
2014-12-11 14:35:43 +0200

    removed AndroidLogger

--- src/org/melato/bus/android/BusDebug.java
+++ src/org/melato/bus/android/BusDebug.java
@@ -1,6 +1,5 @@
 package org.melato.bus.android;
 
-import org.melato.android.log.AndroidLogger;
 import org.melato.log.Log;
 
 import android.content.Context;
@@ -9,7 +8,7 @@ public class BusDebug {
   public static void initLogging(Context context) {
     //File crashFile = new File(Environment.getExternalStorageDirectory(), "buscrash");
     //Thread.setDefaultUncaughtExceptionHandler(new ExceptionHandler(crashFile));
-    Log.setLogger(new AndroidLogger(context));
+    //Log.setLogger(new AndroidLogger(context));
     //File logFile = new File(Environment.getExternalStorageDirectory(), "bus.log");
     //Log.setLogger(new FileLogger(logFile));
     Log.info("initLogging");
